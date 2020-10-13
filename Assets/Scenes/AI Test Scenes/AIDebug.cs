ea992c14c69e6b2e010c98480a80dcb61f8ef84b
JSchoppe <jethro.schoppenhorst@gmail.com>
2020-10-12 23:10:01 -0500

    adds debug materials for guards
    adds basic mats to show current state of guards and a static way of accessing them

---
+++ Assets/Scenes/AI Test Scenes/AIDebug.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AIDebug : MonoBehaviour
+{
+    [SerializeField] private Material stationaryMaterial;
+    [SerializeField] private Material patrollingMaterial;
+    [SerializeField] private Material investigatingMaterial;
+    [SerializeField] private Material chasingMaterial;
+
+    public static Dictionary<AIBehaviorState, Material> debugMats;
+
+    private void Start()
+    {
+        debugMats = new Dictionary<AIBehaviorState, Material>();
+        debugMats.Add(AIBehaviorState.Stationary, stationaryMaterial);
+        debugMats.Add(AIBehaviorState.Patrolling, patrollingMaterial);
+        debugMats.Add(AIBehaviorState.Investigating, investigatingMaterial);
+        debugMats.Add(AIBehaviorState.Chasing, chasingMaterial);
+    }
+}
