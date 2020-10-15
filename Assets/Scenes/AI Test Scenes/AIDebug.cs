c0d3f94327b86e2ba6c848e3cb21d583541f6469
JSchoppe <jethro.schoppenhorst@gmail.com>
2020-10-15 18:21:22 -0500

    cleans up ai debug
    makes inspector more clear for designers

--- Assets/Scenes/AI Test Scenes/AIDebug.cs
+++ Assets/Scenes/AI Test Scenes/AIDebug.cs
@@ -1,21 +1,39 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class AIDebug : MonoBehaviour
+/// <summary>
+/// Contains static references to AI debug assets.
+/// </summary>
+public sealed class AIDebug : MonoBehaviour
 {
-    [SerializeField] private Material stationaryMaterial;
-    [SerializeField] private Material patrollingMaterial;
-    [SerializeField] private Material investigatingMaterial;
-    [SerializeField] private Material chasingMaterial;
+    #region Inspector Fields
+    [Tooltip("The material applied to the debug mesh when a guard is stationary.")]
+    [SerializeField] private Material stationaryMaterial = null;
+    [Tooltip("The material applied to the debug mesh when a guard is patrolling.")]
+    [SerializeField] private Material patrollingMaterial = null;
+    [Tooltip("The material applied to the debug mesh when a guard is investigating.")]
+    [SerializeField] private Material investigatingMaterial = null;
+    [Tooltip("The material applied to the debug mesh when a guard is chasing.")]
+    [SerializeField] private Material chasingMaterial = null;
+    #endregion
+    #region Static Properties
+    /// <summary>
+    /// Contains a debug material for each AI behavior state.
+    /// </summary>
+    public static Dictionary<AIBehaviorState, Material> AIMats { get; private set; }
+    #endregion
 
-    public static Dictionary<AIBehaviorState, Material> debugMats;
-
+    #region Static Properties Initialization
     private void Start()
     {
-        debugMats = new Dictionary<AIBehaviorState, Material>();
-        debugMats.Add(AIBehaviorState.Stationary, stationaryMaterial);
-        debugMats.Add(AIBehaviorState.Patrolling, patrollingMaterial);
-        debugMats.Add(AIBehaviorState.Investigating, investigatingMaterial);
-        debugMats.Add(AIBehaviorState.Chasing, chasingMaterial);
+        // Post the inspector fields to statically accessible properties.
+        AIMats = new Dictionary<AIBehaviorState, Material>
+        {
+            { AIBehaviorState.Stationary, stationaryMaterial },
+            { AIBehaviorState.Patrolling, patrollingMaterial },
+            { AIBehaviorState.Investigating, investigatingMaterial },
+            { AIBehaviorState.Chasing, chasingMaterial }
+        };
     }
+    #endregion
 }
