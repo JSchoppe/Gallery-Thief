b40c8965592886183a966d40a867d843b364fb12
AYogore <AYogore@users.noreply.github.com>
2020-10-18 20:09:59 -0500

    Imported Mixamo and got Idle/Walk working

---
+++ Assets/Scripts/AnimController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour
+{
+    private Animator animator;
+    private GameObject player;
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        player = GetComponent<GameObject>();
+    }
+    void Update()
+    {
+        Walk();
+        
+    }
+
+    void Walk()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //rotate player
+            //start walk animation
+            animator.SetBool("isWalking", true);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("isWalking", true);
+        }
+
+        else
+        {
+            //defaulting to idle animation when no input
+            animator.SetBool("isWalking", false);
+
+        }
+
+
+    }
+}
