badfb1dbeaf9a4ff6e372cfc707c5960bcee0e26
Andy <andyocampo@users.noreply.github.com>
2020-10-20 02:50:29 -0500

    Adds interactive prompt scripts

---
+++ Assets/Scripts/Interactive Prompt/PromptTextController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PromptTextController : MonoBehaviour
+{
+    private TMP_Text promptText;
+    private CanvasGroup canvasGroup;
+
+    private void Start()
+    {
+        promptText = GetComponent<TMP_Text>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnInteractivePromptTriggered(string text, float opacity)
+    {
+        canvasGroup.alpha = opacity;
+        promptText.text = text;
+    }
+}
