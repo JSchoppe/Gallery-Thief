1ef0004442277825797582b1aac174ea6a7dabf3
Andy <andyocampo@users.noreply.github.com>
2020-10-23 04:37:30 -0500

    Adds Settings Menu script, Adds main mixer, audio slider now affects mixer, adds fullscreen toggle and resolution dropdown, adds gold frame for settings panel

---
+++ Assets/Scripts/Menu Scripts/SettingsMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    [SerializeField] AudioMixer audioMixer;
+    [SerializeField] Dropdown resolutionsDropdown;
+
+    Resolution[] resolutions;
+
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionsDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        foreach(Resolution resolution in resolutions)
+        {
+            resolutionsDropdown.options.Add(new Dropdown.OptionData(resolution.ToString()));
+        }
+
+        resolutionsDropdown.RefreshShownValue();
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Screen.SetResolution(resolutions[resolutionsDropdown.value].width, resolutions[resolutionsDropdown.value].height, Screen.fullScreen);
+    }
+}
