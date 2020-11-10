e0ba1269bbb7202f1c2646247f4434d9869fbb65
JSchoppe <jethro.schoppenhorst@gmail.com>
2020-11-09 23:14:20 -0600

    adds extensions
    adds some helper extensions for vector3 and component

---
+++ Assets/Scripts/Cheats/IProgressCheater.cs
@@ -0,0 +1,14 @@
+﻿/// <summary>
+/// Defines a class that can dynamically cheat its progress.
+/// </summary>
+public interface IProgressCheater
+{
+    /// <summary>
+    /// Increases a value or logical step for this class. 
+    /// </summary>
+    void IncrementProgress();
+    /// <summary>
+    /// Decreases a value or logical step for this class. 
+    /// </summary>
+    void DecrementProgress();
+}
