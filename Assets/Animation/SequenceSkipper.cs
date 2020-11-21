8b16774705d2884126fee2e4c2892bb6aab8574f
JSchoppe <jethro.schoppenhorst@gmail.com>
2020-11-21 05:06:43 -0600

    fixes stealables, makes cutscene skippable

---
+++ Assets/Animation/SequenceSkipper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class SequenceSkipper : MonoBehaviour
+{
+    [SerializeField] private PlayableDirector director = null;
+    [SerializeField] private GameObject linkedCanvas = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        director.played += FinishedSequence;
+    }
+
+    private void FinishedSequence(PlayableDirector obj)
+    {
+        StartCoroutine(DeleteNextFrame());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            director.time = 100f;
+            StartCoroutine(DeleteNextFrame());
+        }
+    }
+
+    IEnumerator DeleteNextFrame()
+    {
+        yield return null;
+        Destroy(gameObject);
+        Destroy(linkedCanvas);
+    }
+}
